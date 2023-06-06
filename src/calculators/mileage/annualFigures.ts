93d65aaed8ab276327707d0ccc3ac70d4db5d62f
g4knr <85592362+g4knr@users.noreply.github.com>
2023-06-06 16:25:55 +0200

    Pushed to dist

--- src/calculators/mileage/annualFigures.ts
+++ src/calculators/mileage/annualFigures.ts
@@ -1,5 +1,5 @@
 export interface AnnualFigure {
-  year: '2020-21' | '2021-22' | '2022-23';
+  year: '2020-21' | '2021-22' | '2022-23' | '2023-24';
   pre10: {
     car: number;
     van: number;
@@ -47,6 +47,21 @@ export const annualFigures: AnnualFigure[] = [
   },
   {
     year: '2022-23',
+    pre10: {
+      car: 0.45,
+      van: 0.45,
+      motorcycle: 0.24,
+      bicycle: 0.2,
+    },
+    post10: {
+      car: 0.25,
+      van: 0.25,
+      motorcycle: 0.24,
+      bicycle: 0.2,
+    },
+  },
+  {
+    year: '2023-24',
     pre10: {
       car: 0.45,
       van: 0.45,
--- src/calculators/template/annualFigures.ts
+++ src/calculators/template/annualFigures.ts
@@ -1,5 +1,5 @@
 export interface AnnualFigure {
-  year: '2020-21' | '2021-22' | '2022-23';
+  year: '2020-21' | '2021-22' | '2022-23' | '2023-24';
   figures?: {
     [key: string]: number;
   };
